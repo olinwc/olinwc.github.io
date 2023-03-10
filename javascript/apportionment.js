29a8a33e18749b88373f82cec7e5f81ecaa84a84
Olin William Calvin <calvinolinw@gmail.com>
2023-03-09 21:50:32 -0700

    Read CSV file

--- javascript/apportionment.js
+++ javascript/apportionment.js
@@ -1,25 +1,28 @@
-const csv_text_area = document.getElementById('csv-data');
-const csv_data = csv_text_area.value;
+const csv_data_text_area = document.getElementById('csv-data');
+const submit_button = document.getElementById('submit-btn');
+submit_button.addEventListener('click', () => {
+  const csv_data = csv_data_text_area.value;
+  
+  // Split the CSV data into rows
+  const rows = csv_data.split('\n');
 
-// Split the CSV data into rows
-const rows = csv_data.split('\n');
+  // Extract the headers from the first row
+  const headers = rows[0].split(',');
 
-// Extract the headers from the first row
-const headers = rows[0].split(',');
+  // Create an array of objects representing the data
+  const data = [];
 
-// Create an array of objects representing the data
-const data = [];
+  for (let i = 1; i < rows.length; i++) {
+    const columns = rows[i].split(',');
 
-for (let i = 1; i < rows.length; i++) {
-  const columns = rows[i].split(',');
+    const rowData = {};
 
-  const rowData = {};
+    for (let j = 0; j < columns.length; j++) {
+      rowData[headers[j]] = columns[j];
+    }
 
-  for (let j = 0; j < columns.length; j++) {
-    rowData[headers[j]] = columns[j];
+    data.push(rowData);
   }
 
-  data.push(rowData);
-}
-
-console.log(data);
+  console.log(data);
+});
